9234afe90cc8c84f1859b441d1f1d9b4d7d1c11e
Tol <tolsan42@gmail.com>
2025-11-18 16:37:42 +0700

    Fix responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          darkGreen: "#1e3932",
+          green: "#008248",
+          mint: "#d4e9e2",
+          lightMint: "#f1f8f6",
+          cream: "#f3f1e7",
+          black: "#000000",
+
+        },
+      },
+      fontFamily: {
+        sodo: ["'SoDo Sans'", "sans-serif"],
+      }
+    },
   },
   plugins: [],
 }
